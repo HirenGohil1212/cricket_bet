c1c9ed1013f3722b4ccec99545a9844d18f6c7a9
Hiren Gohil <gohilashvinbhai324@gmail.com>
2025-08-10 05:44:31 +0000

    see this - pls be safe - bcz we will not want tolerate client see this p

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function maskPhoneNumber(phoneNumber: string): string {
+  if (!phoneNumber || phoneNumber.length < 10) {
+    return phoneNumber; // Return original if it's not a valid-looking number
+  }
+  const countryCode = phoneNumber.slice(0, 3); // Assumes +91
+  const lastFour = phoneNumber.slice(-4);
+  const middle = 'X'.repeat(phoneNumber.length - 7);
+  return `${countryCode}${middle}${lastFour}`;
+}
