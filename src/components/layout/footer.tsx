4a1d1e97e6467265a31e937370891540a4ab2110
Hiren Gohil <gohilashvinbhai324@gmail.com>
2025-10-15 14:17:52 +0000

    this images has to be in footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export function Footer() {
   return (
-    <footer className="relative w-full h-24 bg-green-700 text-white">
+    <footer className="relative w-full py-6 bg-green-700 text-white">
       <Image
         src="/footer-bg.png"
         alt="Footer Background"
@@ -11,8 +11,15 @@ export function Footer() {
         objectFit="cover"
         objectPosition="center"
         quality={100}
+        className="opacity-20"
       />
-      <div className="relative z-10 flex items-center justify-center h-full">
+      <div className="relative z-10 container mx-auto px-4 flex flex-col items-center justify-center h-full space-y-4">
+        <div className="flex items-center justify-center gap-4">
+          <Image src="/gt.png" alt="gt logo" width={40} height={40} className="object-contain" />
+          <Image src="/gamecare.png" alt="Gamecare logo" width={40} height={40} className="object-contain" />
+          <Image src="/18plus.png" alt="18+ logo" width={40} height={40} className="object-contain" />
+          <Image src="/ssl.png" alt="SSL Secure logo" width={80} height={40} className="object-contain" />
+        </div>
         <p className="text-xs text-white/80">© Copyright 2025. All Rights Reserved.</p>
       </div>
     </footer>
