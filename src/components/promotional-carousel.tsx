89ece5d5353819075b979e3cb00a46dbb4705ab7
Hiren Gohil <gohilashvinbhai324@gmail.com>
2025-12-11 07:01:07 +0000

    in mobile devie it looks completely banner and good, for big screen you

--- src/components/promotional-carousel.tsx
+++ src/components/promotional-carousel.tsx
@@ -42,7 +42,7 @@ export function PromotionalCarousel({ banners }: PromotionalCarouselProps) {
         {banners.map((banner, index) => (
           <CarouselItem key={banner.id || index}>
             <Card className="overflow-hidden">
-                <div className="relative h-40">
+                <div className="relative h-40 md:h-64">
                   <Image
                     src={banner.imageUrl}
                     alt={`Promotional banner ${index + 1}`}
